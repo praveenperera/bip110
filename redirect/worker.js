f63a314e0f4ea999208219b8d60e056c4e25eba9
Praveen Perera <me@praveenperera.com>
2025-12-26 23:40:05 -0600

    Add og.png image for Open Graph metadata

--- redirect/worker.js
+++ redirect/worker.js
@@ -3,5 +3,5 @@ export default {
     const url = new URL(request.url);
     const redirectUrl = `https://bip110.org${url.pathname}${url.search}`;
     return Response.redirect(redirectUrl, 301);
-  }
-}
+  },
+};
